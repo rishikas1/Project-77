130bfa78650d5e9bbf695a51526921489898e617
rishikas1 <93233769+rishikas1@users.noreply.github.com>
2023-02-12 14:00:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 10;
+console.log(a);
+var b = 5;
+console.log(b);
+
+var add = a + b ;
+console.log(add);
